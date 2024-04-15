dd63cbf62aab8b3daf114cc2eafaac8809495541
ashosive <ash.osive@gmail.com>
2024-04-15 15:20:11 -0400

    first commit

---
+++ truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  contracts_build_directory: './client/src/artifacts',
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+    },
+  },
+  mocha: {
+    // timeout: 100000
+  },
+
+  compilers: {
+    solc: {
+    }
+  },
+
+  db: {
+    enabled: false
+  }
+};
